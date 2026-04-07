5a980ae258e47af9b00fcf542c16110a6f16455d
Sebs_music_live <lopezortegajohansebastian@gmail.com>
2026-04-07 09:36:43 -0500

    Se agrega siteMap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://porfolio-sebas-xi.vercel.app/',
+  
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  
+  integrations: [sitemap()],
 });
